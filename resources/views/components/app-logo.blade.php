405adf5837101234722ff19e48ea48bae47af13d
ArthurWillers <arthurwillers@proton.me>
2025-11-05 18:09:05 -0300

    Add Blade components for app logo, auth header, session status, dropdown, link, nav link, and toast notifications

---
+++ resources/views/components/app-logo.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center">
+    <div class="flex aspect-square size-9 items-center justify-center rounded-md bg-neutral-800">
+        <x-icon name="logo" class="size-7 text-white" />
+    </div>
+    <span class="ms-1 pb-2 truncate text-2xl font-old-london">James</span>
+</div>
