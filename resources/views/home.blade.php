58c9140e4770969b0ddb7cd2e8b67c50bc7f1404
ArthurWillers <arthurwillers@proton.me>
2025-11-05 18:22:50 -0300

    Add home view with welcome message and layout component

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layouts.app>
+    <div class="container mx-auto p-6">
+        <h1 class="text-3xl font-bold mb-4">Bem-vindo ao James!</h1>
+        <p class="text-lg">Esta é a sua página inicial.</p>
+    </div>
+</x-layouts.app>
