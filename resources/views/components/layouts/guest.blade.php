438ec1a84e991b2061c883aa7a69477e802398c4
ArthurWillers <arthurwillers@proton.me>
2025-11-05 15:09:09 -0300

    Add icon component and update guest layout to remove logo icon

--- resources/views/components/layouts/guest.blade.php
+++ resources/views/components/layouts/guest.blade.php
@@ -3,10 +3,7 @@
         <div class="bg-background flex flex-1 flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div class="flex w-full max-w-sm flex-col gap-2">
                 <a href="{{ route('welcome') }}" class="flex flex-col items-center gap-2 font-medium">
-                    <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-lg">
-                        <x-app-logo-icon class="size-9" />
-                    </span>
-                    <span class="sr-only">{{ config('app.name') }}</span>
+                    <h1 class="text-4xl font-old-london text-neutral-900">James</h1>
                 </a>
                 <div class="flex flex-col gap-6">
                     {{ $slot }}
